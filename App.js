2d58aaedf784d6362f444016a3ac76a51c9d0682
mzansidigitalapps <o.amod786@gmail.com>
2026-01-18 17:08:56 +0200

    Initial upload of Mzansi Islamic Calendar

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+export default function App() {
+  return (
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+      <Text>Mzansi Islamic Calendar - Coming Soon</Text>
+    </View>
+  );
+}
